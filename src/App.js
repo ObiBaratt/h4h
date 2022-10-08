f443c9856bc71feee03d39def4248c3515190e49
cassjsdev <cassjes.m@gmail.com>
2022-10-08 16:31:07 -0700

    added component files for login/registration

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import UserInput from './Components/UserInput/UserInput';
+import RegistrationLogin from './Components/RegistrationLogin/RegistrationLogin';
 
 function App() {
   return (
     <div className="App">
       <h1>Team 18's site!!!</h1>
       <UserInput />
+      <RegistrationLogin />
     </div>
   );
 }
---
+++ src/Components/registrationlogin/RegistrationLogin.js
@@ -0,0 +1,12 @@
+import "./RegistrationLogin.css";
+
+export default function RegistrationLogin() {
+    const RegistrationLogin = () => { 
+        return (
+            <div className="RegistrationLogin">
+                <h1>This is the registration and login page.</h1>
+            <RegistrationLogin />
+            </div>
+        );
+    }
+}
