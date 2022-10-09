55eb4993ae6b6bc218d5d11b702b6f3bf470c657
Obi Baratt <obibaratt@gmail.com>
2022-10-08 17:10:01 -0700

    Add Navbar and Page Routing.

---
+++ src/Pages/PageTemplate/Template.js
@@ -0,0 +1,12 @@
+import Navbar from "../../Components/Navbar/Navbar";
+
+const Template = () => {
+    return (
+        <div className="section">
+            <Navbar />
+            <h1>This is a template.</h1>
+        </div>
+    );
+}
+
+export default Template;
