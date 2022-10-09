7714eaea381ea6cff4874373f86b8a5171316714
cassjsdev <cassjes.m@gmail.com>
2022-10-09 00:03:25 -0700

    added styling and colors to register and login

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -2,23 +2,23 @@ import "./Login.css";
 
 const Login = () => { 
     return (
-        <div className="Login" class="p-4">
-            <div className="login-container p-4 w-50 border">
-            <h1>Login</h1>
+        <div className="Login p-4">
+            <div class="login-container p-4 w-50 bg-secondaryy">
+            <h1 className="quaternary">Login</h1>
                 <form>
                 <label>
-                    <p class="fw-bold">Email</p>
-                    <input type="text" placeholder="john.doe123@gmail.com"/>
+                    <p className="fw-bold">Email</p>
+                    <input className="form-rounded border-primaryy bg-tertiary primary" type="text" placeholder="john.doe123@gmail.com"/>
                 </label>
                 <br></br>
                 <br></br>
                 <label>
-                    <p class="fw-bold">Password</p>
-                    <input type="password" placeholder="Password"/>
+                    <p className="fw-bold">Password</p>
+                    <input className="form-rounded border-primaryy bg-tertiary" type="password" placeholder="Password"/>
                 </label>
                 <div>
                     <br></br>
-                    <button type="submit">Submit</button>
+                    <button className="btn bg-quaternary tertiary" type="submit">Submit</button>
                 </div>
                 </form>
                 </div>
